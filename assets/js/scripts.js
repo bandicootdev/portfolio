0c8f25b06c9d615b832b12d6fd17909d5857e3e8
Thaymer portillo <42485564+thaymerapv@users.noreply.github.com>
2021-02-02 23:53:29 -0400

    sending emails through formsubmit, for testing

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -15,4 +15,33 @@
     $btnMenu.lastElementChild.classList.add('none');
     $menu.classList.remove('is-active');
   })
+})(document);
+
+/** Contact Form**/
+((d) => {
+  const $form = d.querySelector('.contact-form')
+  const $loader = d.querySelector('.contact-form-loader')
+  const $response = d.querySelector('.contact-form-response')
+
+  $form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    $loader.classList.remove('none')
+    fetch('https://formsubmit.co/ajax/thaymerapv@gmail.com', {
+      method: "POST",
+      body: new FormData(e.target)
+    }).then((res) => res.ok ? res.json() : Promise.reject(res))
+      .then(() => {
+        location.hash = "#thanks";
+        $form.reset();
+      })
+      .catch(err => {
+        let message = err.statusText || 'An error occurred while sending, please try again'
+        $response.querySelector('h3').innerHTML = `Error ${err.status}: ${message}`
+      }).finally(() => {
+      $loader.classList.add('none')
+      setTimeout(() => {
+        location.hash = '#close'
+      }, 3000)
+    })
+  })
 })(document)
